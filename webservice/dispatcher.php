f5f73a4ff7c9ded76198f734f654115fa1ac710d
Ratikanta Rout <mailtorkrout@gmail.com>
2017-10-02 00:27:08 +0530

    remove unwanted prestashop comments from config files.

--- webservice/dispatcher.php
+++ webservice/dispatcher.php
@@ -1,32 +1,8 @@
 <?php
-/*
-* 2007-2015 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Open Software License (OSL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/osl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2015 PrestaShop SA
-*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
 
 ob_start();
 
-require_once(dirname(__FILE__).'/../config/config.inc.php');
+require_once(dirname(__FILE__) . '/../config/config.inc.php');
 
 // Cart is needed for some requests
 Context::getContext()->cart = new Cart();
@@ -51,7 +27,7 @@ if (isset($_SERVER['PHP_AUTH_USER'])) {
 } elseif (isset($_GET['ws_key'])) {
     $key = $_GET['ws_key'];
 } else {
-    header($_SERVER['SERVER_PROTOCOL'].' 401 Unauthorized');
+    header($_SERVER['SERVER_PROTOCOL'] . ' 401 Unauthorized');
     header('WWW-Authenticate: Basic realm="Welcome to PrestaShop Webservice, please enter the authentication key as the login. No password required."');
     die('401 Unauthorized');
 }
@@ -90,10 +66,9 @@ $result = $request->fetch($key, $method, $_GET['url'], $params, $bad_class_name,
 if (ob_get_length() != 0) {
     header('Content-Type: application/javascript');
 } // Useful for debug...
-
 // Manage cache
 if (isset($_SERVER['HTTP_LOCAL_CONTENT_SHA1']) && $_SERVER['HTTP_LOCAL_CONTENT_SHA1'] == $result['content_sha1']) {
-    $result['status'] = $_SERVER['SERVER_PROTOCOL'].' 304 Not Modified';
+    $result['status'] = $_SERVER['SERVER_PROTOCOL'] . ' 304 Not Modified';
 }
 
 if (is_array($result['headers'])) {
